0451044bd7ba115a4dcaecabd74783c1e70761ae
dhilip1201 <dhilipbeece006@gmail.com>
2020-09-17 19:30:08 +0530

    Updates

---
+++ precache-manifest.de87b75a50cd111e4198315dea1f6a51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab3bde42df6e3e5f8b821b03f5a6951",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "e35e03582175feac83e9",
+    "url": "/covid-19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "78fc9c3a9fd4a076d212",
+    "url": "/covid-19-tracker/static/css/main.b742aeb5.chunk.css"
+  },
+  {
+    "revision": "e35e03582175feac83e9",
+    "url": "/covid-19-tracker/static/js/2.4741842e.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-19-tracker/static/js/2.4741842e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78fc9c3a9fd4a076d212",
+    "url": "/covid-19-tracker/static/js/main.ebc9f693.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  }
+]);
